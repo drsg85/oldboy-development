b3d842105f930051a3c520c62b9ac3889aaee015
nigel-riss <mr.kurenkov@gmail.com>
2019-04-18 11:16:59 +0200

    Add city input autofocus

--- src/js/modules/LocationSelector.js
+++ src/js/modules/LocationSelector.js
@@ -5,16 +5,24 @@ class LocationSelector {
     this.city = document.querySelector('.city-select__city');
     this.citySelector = document.querySelector('.location-selector');
     this.citySelectorClose = document.querySelector('.location-selector__close');
+    this.cityInput = document.querySelector('.city-form__input');
 
     this.reset();
     this.events();
   }
 
+
+  /**
+   * Adds click events to open and close city selector
+   */
   events () {
     this.city.addEventListener('click', (evt) => {
       evt.preventDefault();
 
       this.toggleSelector();
+      setTimeout(() => {
+        this.cityInput.focus();
+      }, 250);
     });
 
     this.citySelectorClose.addEventListener('click', (evt) => {
@@ -24,11 +32,19 @@ class LocationSelector {
     });
   }
 
+
+  /**
+   * Reset city selector state
+   */
   reset () {
     this.citySelector.classList.add('location-selector--hidden');
     this.citySelectorClose.classList.add('location-selector__close--hidden');
   }
-  
+
+
+  /**
+   * Toggle city selector stace (open/closed)
+   */
   toggleSelector () {
     this.citySelector.classList.toggle('location-selector--hidden');
     this.citySelectorClose.classList.toggle('location-selector__close--hidden');
