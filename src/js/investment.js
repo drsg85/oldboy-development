011d7ea3e813f6ed91a29a414426177dd67b37cd
bessistemn1i <eduard.zhulega@yandex.ru>
2019-08-30 15:49:38 +0200

    complete invest-calculator

--- src/js/investment.js
+++ src/js/investment.js
@@ -2,6 +2,7 @@ import InvestForm from './modules/InvestForm';
 import InvestChart from './modules/InvestChart';
 import MobileMenu from './modules/MobileMenu';
 import UpButton from './modules/UpButton';
+import InvestCalculator from './modules/InvestCalculator';
 
 const investForm = new InvestForm();
 
@@ -10,3 +11,5 @@ const investChart = new InvestChart();
 const mobileMenu = new MobileMenu();
 
 const upButton = new UpButton();
+
+const investCalculator = new InvestCalculator();
