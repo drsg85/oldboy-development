b656dea97a4ba5d4515292f39e13a709c643806c
drsg85 <drsg85@gmail.com>
2019-11-12 15:43:39 +0200

    Start make popup

--- src/js/investment.js
+++ src/js/investment.js
@@ -1,16 +1,15 @@
-import InvestForm from './modules/InvestForm';
-import InvestChart from './modules/InvestChart';
+// import InvestChart from './modules/InvestChart';
 import MobileMenu from './modules/MobileMenu';
 import ToTop from './modules/toTop';
 import SmoothOnAnchors from './modules/smoothOnAnchors';
 import InvestCalculator from './modules/InvestCalculator';
-import LocationSelector from './modules/LocationSelector';
-import LocationSearch from './modules/LocationSearch';
+// import LocationSelector from './modules/LocationSelector';
+// import LocationSearch from './modules/LocationSearch';
 import LocationLogo from './modules/LocationLogo';
+import PopUp from './modules/PopUp'
 
-const investForm = new InvestForm();
 
-const investChart = new InvestChart();
+// const investChart = new InvestChart();
 
 const mobileMenu = new MobileMenu();
 
@@ -23,6 +22,8 @@ new SmoothOnAnchors();
 
 const investCalculator = new InvestCalculator();
 
-const locationSelector = new LocationSelector();
+// const locationSelector = new LocationSelector();
 
-const locationSearch = new LocationSearch();
+// const locationSearch = new LocationSearch();
+
+const popUp = new PopUp();
---
+++ src/js/modules/PopUp.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class PopUp {
+    constructor () {
+      this.investmentPopup = document.querySelector('.investment-popup');
+      this.protectionLink = document.querySelector('.protection__link');
+      this.investmentPopupClose = document.querySelector('.investment-popup__close');
+
+      this.events();
+    }
+
+events () {
+    this.protectionLink.addEventListener('click', () => {
+        this.investmentPopup.style.display = "block";
+    });
+
+    this.investmentPopupClose.addEventListener('click', () => {
+        this.investmentPopup.style.display = "none";
+    })
+}
+}
+
+export default PopUp;
