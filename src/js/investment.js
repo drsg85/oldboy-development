8f71b28c1d9336671fb55f49161b8800b3b0e9e7
bessistemn1i <eduard.zhulega@yandex.ru>
2019-11-12 09:44:47 +0200

    remove main.js

--- src/js/investment.js
+++ src/js/investment.js
@@ -1,5 +1,3 @@
-import InvestForm from './modules/InvestForm';
-import InvestChart from './modules/InvestChart';
 import MobileMenu from './modules/MobileMenu';
 import ToTop from './modules/toTop';
 import SmoothOnAnchors from './modules/smoothOnAnchors';
@@ -8,12 +6,7 @@ import LocationSelector from './modules/LocationSelector';
 import LocationSearch from './modules/LocationSearch';
 import LocationLogo from './modules/LocationLogo';
 
-const investForm = new InvestForm();
-
-const investChart = new InvestChart();
-
 const mobileMenu = new MobileMenu();
-
 
 const locationLogo = new LocationLogo();
 
