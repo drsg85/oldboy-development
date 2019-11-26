1afa8bbf54a0ba6a25b31958c058668de0b9c438
bessistemn1i <eduard.zhulega@yandex.ru>
2019-11-26 12:15:10 +0200

    fix form styles for drupal markup. fix overflow in franchise page

--- src/js/investment.js
+++ src/js/investment.js
@@ -1,19 +1,17 @@
-// import InvestChart from './modules/InvestChart';
 import MobileMenu from './modules/MobileMenu';
 import ToTop from './modules/toTop';
 import SmoothOnAnchors from './modules/smoothOnAnchors';
 import InvestCalculator from './modules/InvestCalculator';
-// import LocationSelector from './modules/LocationSelector';
-// import LocationSearch from './modules/LocationSearch';
 import LocationLogo from './modules/LocationLogo';
+import LocationSearch from './modules/LocationSearch';
+import LocationSelector from './modules/LocationSelector';
 import PopUp from './modules/PopUp';
-import Video from './modules/Video';
-
-
-// const investChart = new InvestChart();
 
 const mobileMenu = new MobileMenu();
 
+const locationSelector = new LocationSelector();
+
+const locationSearch = new LocationSearch();
 
 const locationLogo = new LocationLogo();
 
@@ -23,10 +21,4 @@ new SmoothOnAnchors();
 
 const investCalculator = new InvestCalculator();
 
-// const locationSelector = new LocationSelector();
-
-// const locationSearch = new LocationSearch();
-
 const popUp = new PopUp();
-
-const video = new Video();
