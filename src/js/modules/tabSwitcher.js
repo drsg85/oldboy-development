2705d245eb59c113f4a00b5d7bbe0d4b3950efbc
bessistemn1i <eduard.zhulega@yandex.ru>
2019-11-28 16:05:38 +0200

    add tabs to price in branches

---
+++ src/js/modules/tabSwitcher.js
@@ -0,0 +1,45 @@
+class TabSwitcher {
+    constructor(obj) {
+        this.tabs = [...document.querySelectorAll(obj.tabs)];
+        this.elToOpen = [...document.querySelectorAll(obj.elToOpen)];
+        this.classActive;
+        this.addEvents();
+    }
+
+    switchClass(element) {
+        this.tabs.map((el) => el.classList.remove(this.classActive));
+        element.classList.add(this.classActive);
+        this.elToOpen.map((el) => {
+            el.classList.remove('price-list__container--active');
+            setTimeout(() => {
+                el.style.display = 'none';
+            }, 300);
+            if(element.id == el.dataset.content) {
+                el.classList.add('price-list__container--active');
+                setTimeout(() => {
+                    el.style.display = 'block';
+                }, 300);
+            }
+        });
+    }
+
+    addEvents() {
+        const regExp = /^\S+/;
+        const tabClass = this.tabs[0].className.match(regExp)[0];
+        this.classActive = `${tabClass}--active`;
+        this.tabs.map((el, i) => {
+            if(el.classList.contains(this.classActive)) {
+                console.log(el.id);
+                this.elToOpen.map((element) => {
+                    if(el.id == element.dataset.content) {
+                        element.style.display = 'block';
+                        element.classList.add('price-list__container--active');
+                    }
+                })
+            }
+        })
+        this.tabs.map((el) => el.addEventListener('click', () => this.switchClass(el)));
+    }
+}
+
+export default TabSwitcher;
