b6c0dd75f0cffe590be259da0bc36e1ac4bbcc28
bessistemn1i <eduard.zhulega@yandex.ru>
2019-12-26 15:12:26 +0200

    old version

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,9 +13,6 @@ import CheckerLengthOfInputValue from './modules/checerLengthOfInputValue';
 import TabSwitcher from './modules/tabSwitcher';
 import ExclusiveCities from './modules/exclusiveCities';
 import { tns } from '../../node_modules/tiny-slider/src/tiny-slider.js';
-import SmoothOnAnchorsHorizontal from './modules/smoothOnAnchorsHorizontal';
-import HorizontalScroll from './modules/horizontalScroll';
-import GoToElement from './modules/GoToElement';
 
 const mobileMenu = new MobileMenu();
 const locationSelector = new LocationSelector();
@@ -26,7 +23,6 @@ const menuScale = new MenuScale();
 //const geolocationControl = new GeolocationControl();
 //const ipInfo = new IpInfo();
 const checkerLengthOfInputValue = new CheckerLengthOfInputValue();
-const goToElement = new GoToElement();
 
 new ToTop();
 
@@ -52,16 +48,16 @@ if(document.querySelector('.new-format__slider-wrap')) {
     })
 }
 
-if(document.querySelector('.in-countries__slider-container')) {
+if(document.querySelector('.stars__slider-container')) {
     const newFormatSlider = new tns({
         mode: 'carousel',
-        container: '.in-countries__slider',
+        container: '.stars__slider',
         slideBy: 'page',
         mouseDrag: false,
-        gutter: 5,
+        gutter: 10,
         nav: false,
         controls: true,
-        controlsContainer: '.in-countries__slider-controls',
+        controlsContainer: '.stars__slider-controls',
         navPosition: 'bottom',
         items: 1,
         responsive: {
@@ -75,75 +71,10 @@ if(document.querySelector('.in-countries__slider-container')) {
         }
     });
     
-    lightGallery(document.getElementById('in-countries__slider-item-first'));
-    lightGallery(document.getElementById('in-countries__slider-item-second'));
-    lightGallery(document.getElementById('in-countries__slider-item-third'));
-    lightGallery(document.getElementById('in-countries__slider-item-fourth'));
-    lightGallery(document.getElementById('in-countries__slider-item-fifth'));
+    lightGallery(document.getElementById('stars__slider-item-first'));
+    lightGallery(document.getElementById('stars__slider-item-second'));
+    lightGallery(document.getElementById('stars__slider-item-third'));
+    lightGallery(document.getElementById('stars__slider-item-fourth'));
+    lightGallery(document.getElementById('stars__slider-item-fifth'));
 }
 
-if(document.querySelector('.stars__slider-container')) {
-    const starsSlider = new tns({
-        mode: 'carousel',
-        container: '.stars__slider',
-        slideBy: 'page',
-        mouseDrag: true,
-        nav: false,
-        center: true,
-        edgePadding: -45,
-        controls: true,
-        controlsContainer: '.stars__slider-controls',
-        navPosition: 'bottom',
-        items: 1,
-        responsive: {
-            700: {
-                items: 2,
-                edgePadding: 0
-            },
-            
-            1200: {
-                items: 3,
-                slideBy: 3,
-                gutter: 40,
-            }
-        }
-    });
-}
-
-if(document.querySelector('.photo-gallery__slider-container')) {
-    const newFormatSlider = new tns({
-        mode: 'carousel',
-        container: '.photo-gallery__slider',
-        slideBy: 'page',
-        mouseDrag: false,
-        gutter: 0,
-        center: true,
-        edgePadding: -45,
-        nav: false,
-        controls: true,
-        controlsContainer: '.photo-gallery__slider-controls',
-        navPosition: 'bottom',
-        items: 1,
-        responsive: {
-            1000: {
-                gutter: 40
-            }
-        }
-    });
-    
-    lightGallery(document.getElementById('photo-gallery__slider-item-first'));
-    lightGallery(document.getElementById('photo-gallery__slider-item-second'));
-    lightGallery(document.getElementById('photo-gallery__slider-item-third'));
-    lightGallery(document.getElementById('photo-gallery__slider-item-fourth'));
-    lightGallery(document.getElementById('photo-gallery__slider-item-fifth'));
-    lightGallery(document.getElementById('photo-gallery__slider-item-six'));
-    lightGallery(document.getElementById('photo-gallery__slider-item-seven'));
-    lightGallery(document.getElementById('photo-gallery__slider-item-eight'));
-}
-
-new SmoothOnAnchorsHorizontal({
-    triggers: '.smooth-trigger',
-    targets: '.smooth-target'
-});
-
-new HorizontalScroll();
