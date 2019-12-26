bbabb7588c5d482d3f736cdeb722e9e312040c64
bessistemn1i <eduard.zhulega@yandex.ru>
2019-12-26 15:44:59 +0200

    add styles to alphabet buttons

--- src/js/modules/smoothOnAnchorsHorizontal.js
+++ src/js/modules/smoothOnAnchorsHorizontal.js
@@ -58,7 +58,7 @@ class SmoothOnAnchorsHorizontal {
             const reg = /.*(#)/g;
             const href = el.href.match(reg)[0];
             const correct = el.href.replace(href, '');
-
+            
             if(document.querySelector(`#${correct}`) === null) {
                 el.style.color = '#cccccc';
                 el.style.pointerEvents = 'none';
@@ -68,6 +68,23 @@ class SmoothOnAnchorsHorizontal {
             const reg = /.*(#)/g;
             const href = el.href.match(reg)[0];
             const correct = el.href.replace(href, '');
+            this.btns.map((el) => {
+                const reg = /.*(#)/g;
+                const href = el.href.match(reg)[0];
+                const correct = el.href.replace(href, '');
+                
+                if(document.querySelector(`#${correct}`) === null) {
+                    el.style.color = '#cccccc';
+                    el.style.pointerEvents = 'none';
+                }
+                else {
+                    el.style.color = 'initial';
+                    el.style.pointerEvents = 'initial';
+                }
+            })
+            el.style.color = 'red';
+            el.style.pointerEvents = 'none';
+            
             this.scrolling(event, correct);
         }))
     }
