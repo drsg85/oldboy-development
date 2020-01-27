436b7d558a2a5d7bd039dd90be96ce31d68c6b7b
drsg85 <drsg85@gmail.com>
2020-01-27 16:05:05 +0200

    Add more js

--- src/js/modules/Feedback.js
+++ src/js/modules/Feedback.js
@@ -1,40 +1,49 @@
 class Feedback {
     constructor () {
-        this.feedback = document.querySelector('.feedback__container');
-        this.openFeedbackButton = document.querySelector('.member__feedback');
+        this.feedback = document.querySelector('.feedback');
+        this.openFeedbackButton = document.querySelectorAll('.member__feedback');
         this.closeFeedbackButton = document.querySelector('.feedback__close-button');
 
         this.addEvent();
     }
-
+    
     addEvent() {
-        this.openFeedbackButton.addEventListener('click', () => {
-            this.feedback.classList.add('feedback__container--show');
-            this.closeFeedbackButton.classList.add('feedback__close-button--show');
 
-        });
+        for (let i = 0; i < this.openFeedbackButton.length; i++) {
+            let openButton = this.openFeedbackButton[i];
 
-        if (this.feedback.classList.contains('feedback__container')) {
-            console.log('test');
+            console.log();
+
+            openButton.addEventListener('click', () => {
+                console.log(openButton);
+                this.feedback.classList.add('feedback--show');
+                this.closeFeedbackButton.classList.add('feedback__close-button--show');
+            });
+    
+            this.closeFeedbackButton.addEventListener('click', () => {
+                this.feedback.classList.remove('feedback--show');
+                this.closeFeedbackButton.classList.remove('feedback__close-button--show');
+            });
+    
+            document.addEventListener('keydown', (evt) => {
+                if (evt.keyCode === 27) {
+                    this.feedback.classList.remove('feedback--show');
+                    this.closeFeedbackButton.classList.remove('feedback__close-button--show');
+                }
+            });
+    
+            this.openSpace = function () {
+                    if (event.target.closest('feedback__container')) return;
+                    this.feedback.classList.remove('feedback--show');
+            }
+    
             document.addEventListener('click', (event) => {
-                if (event.target.closest('feedback__container')) return;
-                this.feedback.classList.remove('feedback__container--show');
+                if (event.target !== openButton && event.target.classList.contains('feedback')) {
+                this.openSpace();
+            }
             });
         }
 
-        this.closeFeedbackButton.addEventListener('click', () => {
-            this.feedback.classList.remove('feedback__container--show');
-            this.closeFeedbackButton.classList.remove('feedback__close-button--show');
-        });
-
-        document.addEventListener('keydown', (evt) => {
-            if (evt.keyCode === 27) {
-                this.feedback.classList.remove('feedback__container--show');
-                this.closeFeedbackButton.classList.remove('feedback__close-button--show');
-            }
-        });
-
-        
     }
 }
 
