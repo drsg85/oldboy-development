e030b2b595274b9e7c47627e7838eb1863f20c42
bessistemn1i <eduard.zhulega@yandex.ru>
2020-02-27 09:57:09 +0200

    investment features fixes. alphabet fixes

--- src/js/modules/SortBranches.js
+++ src/js/modules/SortBranches.js
@@ -5,41 +5,44 @@ class SortBranches {
     }
 
     addEvents() {
-        const fragment = document.createDocumentFragment();
-        this.branches.map((el) => {
-            const li = [...el.querySelectorAll('.location-list__item')];
-            const obj = li.map((el, i) => {
-                if(el.querySelector('.location-list__metro span') !== null) {
-                    return {
-                        metro: el.querySelector('.location-list__metro span').textContent,
-                        index: i
+        window.onload = () => {
+            const fragment = document.createDocumentFragment();
+            this.branches.map((el) => {
+                const li = [...el.querySelectorAll('.location-list__item')];
+                const obj = li.map((el, i) => {
+                    if(el.querySelector('.location-list__metro span') !== null) {
+                        return {
+                            metro: el.querySelector('.location-list__metro span').textContent,
+                            index: i
+                        }
                     }
-                }
+                })
+    
+                obj.sort((a, b) => {
+                    if(a.metro > b.metro) {
+                        return 1;
+                    }
+                    if(a.metro < b.metro) {
+                        return -1;
+                    }
+                    return 0;
+                });
+                
+                const res = obj.map((el) => {
+                    if(el !== undefined) {
+                        return li[el.index];
+                    }
+                });
+                const result = [...res];
+                result.map((el) => {
+                    if(el !== undefined) {
+                        fragment.appendChild(el);
+                    }
+                })
+                el.appendChild(fragment);
             })
-
-            obj.sort((a, b) => {
-                if(a.metro > b.metro) {
-                    return 1;
-                }
-                if(a.metro < b.metro) {
-                    return -1;
-                }
-                return 0;
-            });
-            
-            const res = obj.map((el) => {
-                if(el !== undefined) {
-                    return li[el.index];
-                }
-            });
-            const result = [...res];
-            result.map((el) => {
-                if(el !== undefined) {
-                    fragment.appendChild(el);
-                }
-            })
-            el.appendChild(fragment);
-        })
+        }
+        
     }
 }
 
