d6625e5d13e4a6ce2691637493e69aa0c7cc8142
bessistemn1i <bessistemn1i@gmail.com>
2020-03-29 22:30:55 +0200

    turn off fetch masters module

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -145,7 +145,8 @@ if(document.querySelector('.photo-gallery__slider-container')) {
 
 function checkWindowWidth() {
     if(window.innerWidth < 700) {
-        const anchors = document.querySelectorAll('.alphabet__trigger');
+        const anchors = [...document.querySelectorAll('.alphabet__trigger')];
+        console.log(anchors);
         anchors.map((el) => {
             const reg = /.*(#)/g;
             const href = el.href.match(reg)[0];
@@ -181,7 +182,7 @@ new HorizontalScroll();
 
 new GoToElement();
 
-new FetchMasters();
+//new FetchMasters();
 
 new SmoothOnAnchors();
 
