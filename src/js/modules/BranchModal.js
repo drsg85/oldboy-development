985693363221f157166ce7e7644a54dfca95993a
bessistemn1i <bessistemn1i@gmail.com>
2020-05-08 19:51:39 +0200

    fix ms-booking button

--- src/js/modules/BranchModal.js
+++ src/js/modules/BranchModal.js
@@ -3,23 +3,16 @@ class BranchModal {
         this.modal = document.querySelector('.branch-modal');
         this.cbtn = document.querySelector('.branch-modal__close');
         this.ybtn = document.querySelector('.yButton');
+        this.popupTrigger = document.querySelector('.popup-trigger');
         this.ybtnSquare = document.querySelector('.ms_booking');
-        this.triggers = [this.ybtn, this.ybtnSquare];
         this.addEvent();
     }
 
     addEvent() {
-        // this.ybtn.classList.remove('yButton');
-        // this.ybtnSquare.classList.remove('ms_booking');
-
-        // this.ybtnSquare.addEventListener('click', (evt) => {
-        //     evt.preventDefault();
-        //     alert('done');
-        // });
-
-        this.triggers.forEach((trigger) => trigger.addEventListener('click', () => {
+        this.popupTrigger.addEventListener('click', () => {
             this.modal.classList.add('branch-modal--visible');
-        }))
+        })
+
 
         this.cbtn.addEventListener('click', () => {
             this.modal.classList.remove('branch-modal--visible');
