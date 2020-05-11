f3123cb5ee52915b65f35fa03945f340e7b74c24
drsg85 <drsg85@gmail.com>
2020-05-11 11:58:30 +0200

    Add weird login for branch buttons

--- src/js/modules/BranchModal.js
+++ src/js/modules/BranchModal.js
@@ -3,26 +3,20 @@ class BranchModal {
         this.modal = document.querySelector('.branch-modal');
         this.cbtn = document.querySelector('.branch-modal__close');
         this.ybtn = document.querySelector('.yButton');
-        this.ybtnSquare = document.querySelector('.ms_booking');
+        this.ybtnSquare = document.querySelector('.button');
         this.triggers = [this.ybtn, this.ybtnSquare];
         this.addEvent();
     }
 
     addEvent() {
-        // this.ybtn.classList.remove('yButton');
-        // this.ybtnSquare.classList.remove('ms_booking');
-
-        // this.ybtnSquare.addEventListener('click', (evt) => {
-        //     evt.preventDefault();
-        //     alert('done');
-        // });
-
-        this.triggers.forEach((trigger) => trigger.addEventListener('click', () => {
-            this.modal.classList.add('branch-modal--visible');
-        }))
-
-        this.cbtn.addEventListener('click', () => {
-            this.modal.classList.remove('branch-modal--visible');
+        this.ybtn.style.visibility = "hidden";
+            this.ybtnSquare.addEventListener('click', () => {
+                this.modal.classList.add('branch-modal--visible');
+            });
+            
+            this.cbtn.addEventListener('click', () => {
+                this.modal.classList.remove('branch-modal--visible');
+                this.ybtn.style.visibility = "visible";
         });
     }
 }
